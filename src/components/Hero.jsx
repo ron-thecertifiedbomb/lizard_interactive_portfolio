8efcb2fb566a8ec839ced9fc6b4d01d5d8cee482
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2023-02-12 17:33:40 +0800

    fixed hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
       </div>
 
       <div className={`hidden md:block ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={herologo} alt="billing" className="w-[100%] h-[auto] relative z-[5]" />
+        <img src={herologo} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
         {/* gradient start */}
         {/* <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
